cf8f961cdeb842f4c480775bba7b3dc6b0c2abcc
DilshanMadhusanka <139458721+DilshanMadhusanka@users.noreply.github.com>
2023-11-26 23:08:34 +0530

    Create an interface

---
+++ CleanMovieApplication/IMovieServices.cs
@@ -0,0 +1,17 @@
+﻿using CleanMovie.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanMovieApplication
+{
+
+
+    // this is use case 
+    public interface IMovieServices
+    {
+        List<Movie> GetAllMovies();
+    }
+}
