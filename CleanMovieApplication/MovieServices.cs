d2e5edd2c6a9ff71e29a2db3f2666487f98cdc1c
DilshanMadhusanka <139458721+DilshanMadhusanka@users.noreply.github.com>
2023-11-26 23:20:10 +0530

    Dependacy Injection

---
+++ CleanMovieApplication/MovieServices.cs
@@ -0,0 +1,28 @@
+﻿using CleanMovie.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanMovieApplication
+{
+    public class MovieServices : IMovieServices
+    {
+        // Constructor Dependancy injection 
+        // to create constructor type => ctor then hit enter
+        public MovieServices( IMovieReporsitory movieReporsitory)
+        {
+            MovieReporsitory = movieReporsitory;
+        }
+
+        public IMovieReporsitory MovieReporsitory { get; }
+
+        public List<Movie> GetAllMovies()
+        {
+            
+            var movies = MovieReporsitory.GetAllMovies();
+           return movies; 
+        }
+    }
+}
